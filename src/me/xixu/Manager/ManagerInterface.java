7fbf90fee53a4b09eadff511004a3c5ca6993cd1
Xi Xu <i@xi-xu.me>
2024-06-18 15:36:14 +0800

    Initial commit

---
+++ src/me/xixu/Manager/ManagerInterface.java
@@ -0,0 +1,96 @@
+package me.xixu.Manager;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+import me.xixu.MainInterface;
+import me.xixu.Static.Information;
+
+public class ManagerInterface extends JPanel implements ActionListener {
+	private JLabel label;
+	private JButton back;
+	private JButton btn_insertBook;
+	private JButton btn_state;
+	private JButton btn_newClass;
+	private JButton btn_updateBook;
+	private static JLabel lb_manager;
+
+	/**
+	 * Create the panel.
+	 */
+	public ManagerInterface() {
+		setBackground(Color.CYAN);
+		setLayout(null);
+
+		label = new JLabel("\u7BA1\u7406\u5458\u754C\u9762");
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		label.setFont(new Font("宋体", Font.BOLD, 40));
+		label.setBounds(530, 20, 294, 105);
+		add(label);
+
+		back = new JButton("\u8FD4\u56DE");
+		back.setFont(new Font("宋体", Font.PLAIN, 26));
+		back.setBounds(0, 0, 115, 82);
+		add(back);
+		back.addActionListener(this);
+
+		btn_insertBook = new JButton("\u5F55\u5165\u65B0\u4E66");
+		btn_insertBook.setFont(new Font("宋体", Font.BOLD, 40));
+		btn_insertBook.setBounds(21, 241, 280, 505);
+		add(btn_insertBook);
+		btn_insertBook.addActionListener(this);
+
+		btn_newClass = new JButton("\u8BBE\u7ACB\u65B0\u7C7B\u522B");
+		btn_newClass.setFont(new Font("宋体", Font.BOLD, 40));
+		btn_newClass.setBounds(347, 241, 280, 505);
+		add(btn_newClass);
+		btn_newClass.addActionListener(this);
+
+		btn_updateBook = new JButton("\u66F4\u65B0\u4FE1\u606F");
+		btn_updateBook.setFont(new Font("宋体", Font.BOLD, 40));
+		btn_updateBook.setBounds(673, 241, 280, 505);
+		add(btn_updateBook);
+		btn_updateBook.addActionListener(this);
+
+		btn_state = new JButton("\u9986\u85CF\u72B6\u6001");
+		btn_state.setFont(new Font("宋体", Font.BOLD, 40));
+		btn_state.setBounds(995, 241, 280, 505);
+		add(btn_state);
+		btn_state.addActionListener(this);
+
+		lb_manager = new JLabel("New label");
+		lb_manager.setHorizontalAlignment(SwingConstants.CENTER);
+		lb_manager.setFont(new Font("宋体", Font.BOLD, 28));
+		lb_manager.setBounds(954, 0, 348, 56);
+		add(lb_manager);
+
+	}
+
+	// 设置右上角当前管理员
+	public static void setManager() {
+		lb_manager.setText("当前管理员：" + Information.manager);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == back) {
+			MainInterface.ManagertoMain();
+		} else if (e.getSource() == btn_insertBook) {
+			MainInterface.ManagertoInsert();
+		} else if (e.getSource() == btn_newClass) {
+			MainInterface.ManagertoNewClass();
+		} else if (e.getSource() == btn_updateBook) {
+			MainInterface.ManagertoUpdate();
+		} else if (e.getSource() == btn_state) {
+			MainInterface.ManagertoState();
+		}
+
+	}
+}
---
+++ src/me/xixu/Manager/State.java
@@ -0,0 +1,87 @@
+package me.xixu.Manager;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import me.xixu.MainInterface;
+import me.xixu.DataBase.TableOperate;
+import me.xixu.Static.Information;
+
+public class State extends JPanel implements ActionListener {
+	private JLabel label;
+	private JButton back;
+	private JTextField tf_classname;
+	private JLabel lb_classname;
+	private JButton btn_search;
+
+	/**
+	 * Create the panel.
+	 */
+	public State() {
+		setBackground(new Color(250, 250, 210));
+		setLayout(null);
+
+		back = new JButton("\u8FD4\u56DE");
+		back.setFont(new Font("宋体", Font.PLAIN, 26));
+		back.setBounds(0, 0, 115, 82);
+		add(back);
+		back.addActionListener(this);
+
+		label = new JLabel("\u56FE\u4E66\u5E93\u72B6\u6001\u67E5\u8BE2");
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		label.setFont(new Font("宋体", Font.BOLD, 40));
+		label.setBounds(530, 20, 294, 105);
+		add(label);
+
+		lb_classname = new JLabel("\u4E66\u5E93\u7C7B\u522B\u540D\u79F0\uFF1A");
+		lb_classname.setFont(new Font("宋体", Font.BOLD, 35));
+		lb_classname.setBounds(241, 330, 283, 55);
+		add(lb_classname);
+
+		tf_classname = new JTextField();
+		tf_classname.setFont(new Font("宋体", Font.BOLD, 35));
+		tf_classname.setColumns(10);
+		tf_classname.setBounds(502, 320, 526, 75);
+		add(tf_classname);
+
+		btn_search = new JButton("\u67E5\u8BE2");
+		btn_search.setFont(new Font("宋体", Font.BOLD, 35));
+		btn_search.setBounds(558, 567, 251, 80);
+		add(btn_search);
+		btn_search.addActionListener(this);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == back) {
+			MainInterface.StatetoManager();
+		} else if (e.getSource() == btn_search) {
+
+			String classname = tf_classname.getText();
+
+			Information.search_classname = classname;
+
+			// 查询该类图书信息
+			// 判定是否存在查询类别
+			if (TableOperate.isExist_Table(classname + "book")) {
+				JOptionPane.showMessageDialog(null, "欢迎", "查询成功", JOptionPane.OK_CANCEL_OPTION);
+				TableOperate.search_classname(tf_classname.getText());
+				MainInterface.StatetoStateInfo();
+				State_Information.setTextArea();
+				tf_classname.setText("");
+			} else {
+				JOptionPane.showMessageDialog(null, "不存在该类别", "查询失败", JOptionPane.ERROR_MESSAGE);
+			}
+		}
+	}
+}
