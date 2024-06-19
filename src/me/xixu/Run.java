ebc339e07c8c1fe644169573d0d87b8f6b2cf427
Xi Xu <i@xi-xu.me>
2024-06-19 21:27:26 +0800

    Update Run.java

--- src/me/xixu/Run.java
+++ src/me/xixu/Run.java
@@ -3,14 +3,11 @@ package me.xixu;
 import java.awt.EventQueue;
 
 public class Run {
-
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					// 实例化主界面
 					MainInterface frame = new MainInterface();
-					// 显示主界面
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
